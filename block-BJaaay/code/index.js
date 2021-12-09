d9a2e16895130108aa0e8a5672c8390ee42412ee
nmn <nmngarg030798@gmail.com>
2021-12-09 21:56:14 +0530

    add

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,33 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (let i = 0; i < 11; i = i + 1 ) {
+    console.log (i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
+let i = 0;
+while (i < 10) {
+    i = i + 1;
+    if (i % 2 == 0){ console.log (i)};
+}
+
 
 // Using `console.log` log all the od values from 1 to 10.
+i = 0;
+while (i < 10) {
+    i = i + 1;
+    if (i % 2 !== 0){ console.log (i)};
+}
+
 
 // Calculate the sum of all numbers from 1 to 10.
 
+console.log(1+2+3+4+5+6+7+8+9+10);
+
 // Log all the values from 1 to 10 using while loop
+i = 0;
+while (i < 10) {
+    i = i + 1;
+    console.log (i);
+}
