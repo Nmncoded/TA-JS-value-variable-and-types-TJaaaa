30435b6b5eee5971c2a7575470a2cf6ef0d48e63
nmn <nmngarg030798@gmail.com>
2021-12-09 22:03:55 +0530

    add

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -15,11 +15,17 @@ while (i < 10) {
     if (i % 2 == 0){ console.log (i)};
 }
 
+for (let i = 1; i <= 10; i = i + 1 ) {
+    if (i % 2 === 0){ console.log (i)};
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 i = 0;
 while (i < 10) {
     i = i + 1;
+    if (i % 2 !== 0){ console.log (i)};
+}
+for (let i = 1; i <= 10; i = i + 1 ) {
     if (i % 2 !== 0){ console.log (i)};
 }
 
